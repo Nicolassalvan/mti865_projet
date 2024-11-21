df346a63de8c257a8efda89c118b4970469e8874
Tim <git@timvoigt.dev>
2024-11-21 13:03:47 -0500

    Add visualization of predictions

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 import numpy as np
 import torch
 import torch.nn as nn
@@ -19,7 +20,8 @@ import scipy.spatial
 # from scipy.spatial.distance import directed_hausdorff
 
 
-labels = {0: 'Background', 1: 'Foreground'}
+labels = {0: "Background", 1: "Foreground"}
+LABEL_TO_COLOR = {0: [0, 0, 0], 1: [255, 0, 0], 2: [0, 255, 0], 3: [0, 0, 255]}
 
 
 def compute_dsc(pred, gt):
@@ -37,7 +39,9 @@ def compute_dsc(pred, gt):
 
 def getImageImageList(imagesFolder):
     if os.path.exists(imagesFolder):
-        imageNames = [f for f in os.listdir(imagesFolder) if isfile(join(imagesFolder, f))]
+        imageNames = [
+            f for f in os.listdir(imagesFolder) if isfile(join(imagesFolder, f))
+        ]
         imageNames.sort()
 
     return imageNames
@@ -61,6 +65,32 @@ def predToSegmentation(pred):
     return (x == 1).float()
 
 
+def plot_net_predictions(imgs, true_masks, masks_pred, batch_size):
+
+    fig, ax = plt.subplots(3, batch_size, figsize=(20, 15))
+
+    for i in range(batch_size):
+
+        img = np.transpose(imgs[i].cpu().detach().numpy(), (1, 2, 0))
+        mask_pred = masks_pred[i].cpu().detach().numpy()
+        mask_true = np.transpose(true_masks[i].cpu().detach().numpy(), (1, 2, 0))
+
+        ax[0, i].imshow(img)
+        ax[1, i].imshow(mask_to_rgb(mask_pred))
+        ax[1, i].set_title("Predicted")
+        ax[2, i].imshow(mask_true)
+        ax[2, i].set_title("Ground truth")
+
+    return fig
+
+
+def mask_to_rgb(mask):
+    rgb = np.zeros(mask.shape + (3,), dtype=np.uint8)
+    for i in np.unique(mask):
+        rgb[mask == i] = LABEL_TO_COLOR[i]
+    return rgb
+
+
 def getTargetSegmentation(batch):
     # input is 1-channel of values between 0 and 1
     # values are as follows : 0, 0.33333334, 0.6666667 and 0.94117647
@@ -68,9 +98,6 @@ def getTargetSegmentation(batch):
 
     denom = 0.33333334  # for ACDC this value
     return (batch / denom).round().long().squeeze()
-
-
-from scipy import ndimage
 
 
 def inference(net, img_batch, modelName, epoch):
@@ -83,7 +110,9 @@ def inference(net, img_batch, modelName, epoch):
     losses = []
     for i, data in enumerate(img_batch):
 
-        printProgressBar(i, total, prefix="[Inference] Getting segmentations...", length=30)
+        printProgressBar(
+            i, total, prefix="[Inference] Getting segmentations...", length=30
+        )
         images, labels, img_names = data
 
         images = to_var(images)
@@ -96,14 +125,22 @@ def inference(net, img_batch, modelName, epoch):
         pred_y = softMax(net_predictions)
         masks = torch.argmax(pred_y, dim=1)
 
-        path = os.path.join('./Results/Images/', modelName, str(epoch))
+        path = os.path.join("./Results/Images/", modelName, str(epoch))
 
         if not os.path.exists(path):
             os.makedirs(path)
 
         torchvision.utils.save_image(
-            torch.cat([images.data, labels.data, masks.view(labels.shape[0], 1, 256, 256).data / 3.0]),
-            os.path.join(path, str(i) + '.png'), padding=0)
+            torch.cat(
+                [
+                    images.data,
+                    labels.data,
+                    masks.view(labels.shape[0], 1, 256, 256).data / 3.0,
+                ]
+            ),
+            os.path.join(path, str(i) + ".png"),
+            padding=0,
+        )
 
     printProgressBar(total, total, done="[Inference] Segmentation Done !")
 
@@ -115,4 +152,3 @@ def inference(net, img_batch, modelName, epoch):
 class MaskToTensor(object):
     def __call__(self, img):
         return torch.from_numpy(np.array(img, dtype=np.int32)).float()
-
