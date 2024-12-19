f9bb9a583894235f8891df43a6ce563e66c9fd3b
MatIdt <mathieudat1@gmail.com>
2024-12-18 22:05:45 -0500

    Ajout des métriques precision, accuracy, recall, f1 et AUC coeff

--- metrics.py
+++ metrics.py
@@ -17,14 +17,16 @@ from PIL import Image
 from medpy.metric.binary import dc, hd, asd, assd, jc
 import scipy.spatial
 from torch import tensor
-from torchmetrics.functional.classification import binary_jaccard_index
+from torchmetrics.functional.classification import binary_jaccard_index, binary_precision,binary_accuracy, binary_recall, binary_f1_score, binary_auroc
 import sklearn.metrics
 import math
 from utils import to_var,getTargetSegmentation
 # from scipy.spatial.distance import directed_hausdorff
 
 
-def Dice_score_class(model,dataloader,device,version):
+# Fonction pour la métrique dice score en utilisant un calcul en dur des intersections et 
+# dénominateur sans smooth. Renvoie un tableau contenant quatre dice score (un par classe)
+def Dice_score_class(model,dataloader,device):
     dice_class=[0,0,0,0]
     num_classes=4
     smooth=1e-6
@@ -45,41 +47,16 @@ def Dice_score_class(model,dataloader,device,version):
             for classe in range (num_classes):
                 # Calcule l'intersection et le denominateur pour le Dice score
                 pred_class = np.zeros(y_pred_batch.shape)
-                if(version!=3):
-                    idx_pred = np.where(y_pred_batch == classe)
-                    pred_class[idx_pred] = 1
-                else :
-                    for i in range (y_pred_batch.shape[0]):
-                        for j in range (y_pred_batch.shape[1]):
-                            if (y_pred_batch[i][j] == classe):
-                                pred_class[i][j] = probs[i_batch][classe][i][j].item()
+                idx_pred = np.where(y_pred_batch == classe)
+                pred_class[idx_pred] = 1
                 
-                if(version==1 or version ==2): # Calcul en dur du score dice
-                    intersect = (pred_class*dice_target[i_batch][classe].data.numpy()).sum()#+ smooth
-                    den = (((pred_class*pred_class).sum())+((dice_target[i_batch][classe].data.numpy()*dice_target[i_batch][classe].data.numpy()).sum()))#+ smooth
-                    
-                    if (version ==2):# Calcul en dur sans utiliser les smooth
-                        #intersect=intersect-smooth
-                        dice_classe=2.0 * (intersect / den)
-                        if math.isnan(dice_classe): # utile si on n'utilise pas de smooth
-                            dice_classe=0
-                    else :
-                        intersect=intersect+smooth
-                        den=den+smooth
-                        dice_classe=2.0 * (intersect / den)
-
+                intersect = (pred_class*dice_target[i_batch][classe].data.numpy()).sum()#+ smooth
+                den = (((pred_class*pred_class).sum())+((dice_target[i_batch][classe].data.numpy()*dice_target[i_batch][classe].data.numpy()).sum()))#+ smooth
                 
-                # Version qui prend plus longtemps à se calculer
-                elif (version ==3): # Calcul en dur du score soft dice, ce n'est plus ŷp mais ŝp (softmax au lieu de binaire 0 ou 1)
-                    intersect = (pred_class*dice_target[i_batch][classe].data.numpy()).sum()#+ smooth
-                    den = (((pred_class*pred_class).sum())+((dice_target[i_batch][classe].data.numpy()*dice_target[i_batch][classe].data.numpy()).sum()))#+ smooth
-                    
-                    dice_classe=(2.0 * (intersect / den))
-                    if math.isnan(dice_classe): # utile seulement si on n'utilise pas de smooth dans intersect et den
-                        dice_classe=0
+                dice_classe=2.0 * (intersect / den)
+                if math.isnan(dice_classe): # utile si on n'utilise pas de smooth
+                    dice_classe=0
 
-                elif (version ==4):# Calcul dice score avec medpy.binary.dc
-                    dice_classe= dc(pred_class, dice_target[i_batch][classe].data.numpy())
 
                 dice_class[classe]+= dice_classe
             nb_it+=1
@@ -89,7 +66,9 @@ def Dice_score_class(model,dataloader,device,version):
         dice_class[classe]=dice_class[classe]/nb_it
     return dice_class
 
-def Jaccard_score_class(model,dataloader,device,version): 
+# Fonction pour la métrique jaccard score en utilisant binary_jaccard_index de torchmetrics. 
+# Renvoie un tableau contenant quatre jaccard score (un par classe) 
+def Jaccard_score_class(model,dataloader,device): 
     jaccard=[0,0,0,0]
     num_classes=4
     smooth=1e-6
@@ -113,29 +92,7 @@ def Jaccard_score_class(model,dataloader,device,version):
                 idx_pred = np.where(y_pred_batch == classe)
                 pred_class[idx_pred] = 1
                 
-                if (version==1):# Calcul en dur du jaccard score avec smooth
-                    intersect = (np.multiply(pred_class,jaccard_target[i_batch][classe].data.numpy())).sum()+smooth
-                
-                    den = ((np.multiply(pred_class,pred_class)) + (np.multiply(jaccard_target[i_batch][classe].data.numpy(), jaccard_target[i_batch][classe].data.numpy()))).sum()+smooth
-                    den5=den-intersect+smooth
-
-                    jaccard_classe=(intersect / den5)
-
-                elif(version==2):# Calcul en dur du jaccard score sans smooth
-                    intersect = (np.multiply(pred_class,jaccard_target[i_batch][classe].data.numpy())).sum()
-                
-                    den = ((np.multiply(pred_class,pred_class)) + (np.multiply(jaccard_target[i_batch][classe].data.numpy(), jaccard_target[i_batch][classe].data.numpy()))).sum()
-                    den5=den-intersect
-
-                    jaccard_classe=(intersect / den5)
-                    if math.isnan(jaccard_classe): # utile si on n'utilise pas de smooth dans intersect et den
-                        jaccard_classe=0
-
-                elif (version==3):# Calcul du jaccard score avec jc de medpy.binary
-                    jaccard_classe= jc(pred_class, jaccard_target[i_batch][classe].data.numpy())
-
-                elif (version==4):# Calcul du jaccard score avec binary_jaccard_index de torchmetrics
-                    jaccard_classe= binary_jaccard_index(tensor(pred_class), jaccard_target[i_batch][classe]).item()
+                jaccard_classe= binary_jaccard_index(tensor(pred_class), jaccard_target[i_batch][classe]).item()
             
                 jaccard[classe]+= jaccard_classe
             nb_it+=1
@@ -145,7 +102,8 @@ def Jaccard_score_class(model,dataloader,device,version):
     return jaccard
 
 
-# Fonction pour la métrique HSD (disatnce Hausdorff) en utilisant hd from medpy 
+# Fonction pour la métrique HSD (disatnce Hausdorff) en utilisant hd de medpy. Renvoie un tableau
+# contenant quatre HSD score (un par classe)
 def hsd_score_class(model,dataloader,device):  
 
     hsd=[0,0,0,0]
@@ -186,3 +144,186 @@ def hsd_score_class(model,dataloader,device):
     # L'unité de distance est la même pour l'espacement des éléments dans chaque dimension, 
     # qui est généralement donné en mm.
     return hsd
+
+
+# Fonction pour la métrique precision en utilisant binary_precision de torchmetrics. Renvoie un 
+# tableau contenant quatre precision score (un par classe) 
+def precision_class(model,dataloader,device):  
+
+    precision=[0,0,0,0]
+    num_classes=4
+    nb_it=0
+    for idx, data in enumerate(dataloader):
+        images, labels, _ = data
+
+        labels = to_var(labels).to(device)
+        images = to_var(images).to(device)
+
+        net_predictions = model(images)
+        probs = torch.softmax(net_predictions, dim=1)
+
+        segmentation_classes = getTargetSegmentation(labels)
+        precision_target = F.one_hot(segmentation_classes, num_classes = num_classes).permute(0,3,1,2).contiguous()
+
+        for i_batch in range (precision_target.shape[0]):
+            y_pred_batch = torch.argmax(probs[i_batch], dim=0)
+            for classe in range (num_classes):
+                pred_class = np.zeros(y_pred_batch.shape)
+                idx_pred = np.where(y_pred_batch == classe)
+                pred_class[idx_pred] = 1
+
+                precision_classe=binary_precision(tensor(pred_class), precision_target[i_batch][classe]).item()
+                precision[classe]+= precision_classe
+
+            nb_it+=1
+
+    for classe in range (num_classes):
+        precision[classe]=precision[classe]/nb_it
+
+    return precision
+
+
+# Fonction pour la métrique accuracy en utilisant binary_accuracy de torchmetrics. Renvoie un 
+# tableau contenant quatre accuracy score (un par classe) 
+def accuracy_class(model,dataloader,device):  
+
+    accuracy=[0,0,0,0]
+    num_classes=4
+    nb_it=0
+    for idx, data in enumerate(dataloader):
+        images, labels, _ = data
+
+        labels = to_var(labels).to(device)
+        images = to_var(images).to(device)
+
+        net_predictions = model(images)
+        probs = torch.softmax(net_predictions, dim=1)
+
+        segmentation_classes = getTargetSegmentation(labels)
+        accuracy_target = F.one_hot(segmentation_classes, num_classes = num_classes).permute(0,3,1,2).contiguous()
+
+        for i_batch in range (accuracy_target.shape[0]):
+            y_pred_batch = torch.argmax(probs[i_batch], dim=0)
+            for classe in range (num_classes):
+                pred_class = np.zeros(y_pred_batch.shape)
+                idx_pred = np.where(y_pred_batch == classe)
+                pred_class[idx_pred] = 1
+
+                accuracy_classe=binary_accuracy(tensor(pred_class), accuracy_target[i_batch][classe]).item()
+                accuracy[classe]+= accuracy_classe
+
+            nb_it+=1
+
+    for classe in range (num_classes):
+        accuracy[classe]=accuracy[classe]/nb_it
+
+    return accuracy
+
+# Fonction pour la métrique recall en utilisant binary_recall de torchmetrics. Renvoie un tableau
+# contenant quatre recall score (un par classe)
+def recall_class(model,dataloader,device):  
+
+    recall=[0,0,0,0]
+    num_classes=4
+    nb_it=0
+    for idx, data in enumerate(dataloader):
+        images, labels, _ = data
+
+        labels = to_var(labels).to(device)
+        images = to_var(images).to(device)
+
+        net_predictions = model(images)
+        probs = torch.softmax(net_predictions, dim=1)
+
+        segmentation_classes = getTargetSegmentation(labels)
+        recall_target = F.one_hot(segmentation_classes, num_classes = num_classes).permute(0,3,1,2).contiguous()
+
+        for i_batch in range (recall_target.shape[0]):
+            y_pred_batch = torch.argmax(probs[i_batch], dim=0)
+            for classe in range (num_classes):
+                pred_class = np.zeros(y_pred_batch.shape)
+                idx_pred = np.where(y_pred_batch == classe)
+                pred_class[idx_pred] = 1
+
+                recall_classe=binary_recall(tensor(pred_class), recall_target[i_batch][classe]).item()
+                recall[classe]+= recall_classe
+
+            nb_it+=1
+
+    for classe in range (num_classes):
+        recall[classe]=recall[classe]/nb_it
+
+    return recall
+
+
+# Fonction pour la métrique f1 score en utilisant binary_f1_score de torchmetrics. Renvoie un 
+# tableau contenant quatre f1 score (un par classe)
+def f1_score_class(model,dataloader,device):  
+
+    f1_score=[0,0,0,0]
+    num_classes=4
+    nb_it=0
+    for idx, data in enumerate(dataloader):
+        images, labels, _ = data
+
+        labels = to_var(labels).to(device)
+        images = to_var(images).to(device)
+
+        net_predictions = model(images)
+        probs = torch.softmax(net_predictions, dim=1)
+
+        segmentation_classes = getTargetSegmentation(labels)
+        f1_score_target = F.one_hot(segmentation_classes, num_classes = num_classes).permute(0,3,1,2).contiguous()
+
+        for i_batch in range (f1_score_target.shape[0]):
+            y_pred_batch = torch.argmax(probs[i_batch], dim=0)
+            for classe in range (num_classes):
+                pred_class = np.zeros(y_pred_batch.shape)
+                idx_pred = np.where(y_pred_batch == classe)
+                pred_class[idx_pred] = 1
+
+                f1_score_classe=binary_f1_score(tensor(pred_class), f1_score_target[i_batch][classe]).item()
+                f1_score[classe]+= f1_score_classe
+
+            nb_it+=1
+
+    for classe in range (num_classes):
+        f1_score[classe]=f1_score[classe]/nb_it
+
+    return f1_score
+
+# Fonction pour la métrique AUC coeff en utilisant binary_auroc de torchmetrics. Renvoie un 
+# tableau contenant quatre AUC coeff (un par classe) 
+def auc_coeff_class(model,dataloader,device):  
+
+    auc_coeff=[0,0,0,0]
+    num_classes=4
+    nb_it=0
+    for idx, data in enumerate(dataloader):
+        images, labels, _ = data
+
+        labels = to_var(labels).to(device)
+        images = to_var(images).to(device)
+
+        net_predictions = model(images)
+        probs = torch.softmax(net_predictions, dim=1)
+
+        segmentation_classes = getTargetSegmentation(labels)
+        auc_coeff_target = F.one_hot(segmentation_classes, num_classes = num_classes).permute(0,3,1,2).contiguous()
+
+        for i_batch in range (auc_coeff_target.shape[0]):
+            y_pred_batch = torch.argmax(probs[i_batch], dim=0)
+            for classe in range (num_classes):
+                pred_class = np.zeros(y_pred_batch.shape)
+                idx_pred = np.where(y_pred_batch == classe)
+                pred_class[idx_pred] = 1
+
+                auc_coeff_classe=binary_auroc(tensor(pred_class), auc_coeff_target[i_batch][classe]).item()
+                auc_coeff[classe]+= auc_coeff_classe
+
+            nb_it+=1
+
+    for classe in range (num_classes):
+        auc_coeff[classe]=auc_coeff[classe]/nb_it
+
+    return auc_coeff
